301ff04199bf1fd175a1a52efa2312ed7c0f940f
Kellen Busby <kellenbusby@gmail.com>
2021-10-07 20:57:57 -0600

    styling buttons

--- src/components/ControlPanel.js
+++ src/components/ControlPanel.js
@@ -9,8 +9,10 @@ const ControlPanel = ({stops, setFilter}) => {
     <div className='control-panel-container'>
       <h1>Appointments</h1>
       <h3>Filters</h3>
-      <button onClick={handleFilterClick}>Today</button>
-      <button onClick={handleFilterClick}>Tomorrow</button>
+      <div className='btns'>
+        <button onClick={handleFilterClick}>Today</button>
+        <button onClick={handleFilterClick}>Tomorrow</button>
+      </div>
       <h3>Stops</h3>
       {stops && stops.map((stop, idx) => <div key={idx}>{stop.name}</div>)}
       {!stops && <p>No stops scheduled</p>}
